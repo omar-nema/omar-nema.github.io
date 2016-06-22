d2e1745f319fd2554f7994e6a0d8bb98196eefe1
Omar Nema <omarwnema@gmail.com>
2016-06-22 08:52:50 -0400

    working card interactivity

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,15 @@ var breakpointMid = 850;
 var breakpoint2 = 750;
 var numCards = 3;
 //percentage of cardholder width to pass before switching cards
-var fractionIncrement = .15;
+var fractionIncrement = .1;
+
+//need to rethink fractionIncrement
+//
+
+var hoverStyle = {
+    'background-color': 'yellow'
+};
+//reverStyle and setStyle functions
 
 //var initialLoad = true;
 
@@ -25,6 +33,18 @@ var generateLayout = function(){
     };
 };
 
+    var getCardInSequence = function(card, direction){
+        var nextOrder = parseInt(card.attr('order')) - Math.sign(parseInt(direction));
+        var title = card.attr('title');
+    
+        if (nextOrder > 0 && nextOrder < 4){
+            var nextCard = $('.'+ title).find("div[order=" + nextOrder + "]");
+            return nextCard;          
+        }
+        else {
+            return card;
+        };
+    };
 
 
 $(window).resize(function(event){
@@ -39,68 +59,78 @@ $(document).ready(function(event){
             //all functions dependent on intial CSS property load below
         });    
     });
-    $('.about-button').click(function(event){
-        $('.about-page').slideDown(800);   
-    });
-        
-
     
-
-    //edge cases: round2 (not that imp?), card before cardholder ()
-    //jumps over gap and enters cardholder within card - solved w mousemove but sometimes runs 2 times in parallel
-
+  
+    
+    $('.about-button').click(function(event){
+        $('.about-page').slideDown(800); 
+        $('.' + $('.content').attr('class').replace(' ', '.')).css('display', 'none');        
+    });
+    
+    
+        
+    var currCard;  
+        
     $('.cardholder').mouseenter(function(event){
             console.log('ENTER');
             var prevX;
-            var currCard;
             var prevMove, currMove;
             var next;
             var initialX;
             var prevDirection;
             var currHolder = $(this);
-            //we keep track of increments of .05
-            //send the X's to a function
-            $('.card').one('mousemove', function(event2){
+        
+            $('.card').one('mousemove', function(event2){            //MAKE SURE THIS RUNS JUST ONCE!!
+ 
                 $('.card').unbind('mousemove');             
                 initialX = parseFloat(event.pageX) - parseFloat($(currHolder).offset().left);
                 currCard = $(this); //enter selects child
-                $(currHolder).mousemove(function(event){               
+//                //style curr card here?
+                
+                $(currHolder).mousemove(function(event){   
+//                    var currCardClass = 'card' + currCard.attr('order') + 'select';
+                    console.log(currCard.attr('default'));
+                    currCard.removeClass(currCard.attr('default'));
+                    currCard.addClass('card-selector');                    
+                    
                     var currX = parseFloat(event.pageX) - parseFloat($(this).offset().left);  
-                    var currDirection = parseFloat(currX - prevX);  
+                    var currDirection = parseFloat(currX - prevX); 
+                    //update initialX if direction switched
                     if (prevX && prevDirection){   
-                            var directionSwitch = Math.sign(currDirection)*Math.sign(prevDirection)==-1;
-                            if (directionSwitch){
-                                initialX = prevX;
-                            };                         
+                        var directionSwitch = Math.sign(currDirection)*Math.sign(prevDirection)==-1;
+                        if (directionSwitch){
+                            initialX = prevX;
+                            console.log('switchup!');
+                        };                         
                     };
+                    //detect if mouse movement has passed critical threshold to flip cards
                     var currMove = (currX-initialX)/parseFloat($(this).width());
                     var criticalMove = Math.abs(currMove) > fractionIncrement;    
                     if (criticalMove){
-                      initialX = currX;  
-                        console.log('GOT IT');
+                        initialX = currX;      
+                        //new card coming in
+                        if (currCard !== getCardInSequence(currCard, currDirection) ){
+                            currCard.removeClass('card-selector');
+                            currCard.addClass(currCard.attr('default'));
+                            currCard = getCardInSequence(currCard, currDirection);
+                            currCard.removeClass(currCard.attr('default'));
+                            currCard.addClass('card-selector');
+                        }                                    
                     };
-//                    console.log(prevMove, currMove);
-                     
-//                    if (currMove > 0 && criticalMove){ //moving right
-////                        console.log('right critical');
-//                    }
-//                    else if (currMove < 0 && criticalMove){ //moving left
-////                        console.log('left critical');                        
-//                    };
-                    
                     if (prevX) {
                         prevDirection = parseFloat(currDirection);
                     };
                     prevX = currX;
                     prevMove = currMove;
-         
-
                 });    
-            });///card
-
-   
+                
+            });
         }).mouseleave(function(event){ //stop listening
             console.log('EXIT');
+            if (currCard){
+                currCard.removeClass('card-selector');
+                currCard.addClass(currCard.attr('default'));
+            }
             currCard = null;
             $('.cardholder').unbind('mousemove');
             $('.card').unbind('mouseenter');
@@ -108,76 +138,7 @@ $(document).ready(function(event){
     
 });
 
-
-//                        if (Math.sign(currDirection)*Math.sign(prevDirection)==1)
-//                        {
-//                            directionSwitch = true;
-//                            initialX = prevX;
-////                            console.log('directionswitch', directionSwitch);
-//                        };   
-                        
-//                        currCard.css('background-color', 'yellow');
-//                        if (currX > prevX){
-//    //                        getCardInSequence(currCard, 1)
-//                            currMovingRight = true;
-//                        }
-//                        else if (currX < prevX){
-//    //                        getCardInSequence(currCard, -1)                    
-//                            currMovingRight = false;   
-//                        };        
-
-
-
-
-
-//    var getCardInSequence = function(card, next){
-//        
-//        var order = parseInt(card.attr('order'));
-//        var title = card.attr('title');
-//        var nextCard;
-//        //reached end of stack
-//        if ( (order == 3 && next==-1) || (order == 1 && next==1)  ){
-////           return null;
-//        }
-//        else {
-//            var ordernext = parseInt(order) - parseInt(next);
-//            nextCard = $('.'+ title).find("div[order=" + ordernext + "]");
-////            currCard.css('background-color', 'blue');
-//            currCard = nextCard;
-//        };
-//    };
-//    
-
-
-
-//    
-//    $('.cardholder').mouseover(function(event){
-//           
-//        $('.cardholder').mousemove(function(event){
-//            var newX = event.pageX - $(this).offset().left;
-//            var newY = event.pageY - $(this).offset().top;
-//            if (!currX){
-//                currX = newX;
-//            }
-//            if (currCard){
-//                currCard.css('background-color', 'yellow');
-//                if (newX > currX){
-//                    getCardInSequence(currCard, 1)
-//                    movingRight = true;
-//                    movingLeft = false;
-//                }
-//                else if (newX < currX){
-//                    getCardInSequence(currCard, -1)                    
-//                    movingRight = false;
-//                    movingLeft = true;       
-//                }; 
-//            }
-//            currX = newX;
-//
-//        });
-//        //fires multiple times before leave event!
-//
-//        
-//    });
     
-    
+
+    //edge cases: round2 (not that imp?), card before cardholder ()
+    //jumps over gap and enters cardholder within card - solved w mousemove but sometimes runs 2 times in parallel

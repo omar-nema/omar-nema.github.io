4b094d673774387c002da2d50d0813743ebab330
Omar Nema <omarwnema@gmail.com>
2016-07-26 09:29:28 -0400

    another mobile test

--- _site/assets/scripts/main.js
+++ _site/assets/scripts/main.js
@@ -189,46 +189,6 @@ function cardExit(){
 };
 
 
-var xDown = null;                                                        
-var yDown = null;                                                        
-
-function handleTouchStart(evt) {     
-//    console.log('ontouchstart' in document.documentElement);
-    xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-    
-    console.log( evt.touches[0].clientX);
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-        } else {
-            /* right swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
-            /* down swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
-
 
 function aboutTimeLineMouseover() {
     var phaseOrder = $(this).data('phaseorder');
@@ -294,15 +254,21 @@ $(document).ready(function(event){
     $('.cardholder').mouseenter(cardEnter).mouseleave(cardExit);
     //changed to move accidentally?
     
+    console.log(isTouchDevice);
+    //cardholder is not a link
+    
+    //only on touch device
     
 //    if (isTouchDevice){
-//        console.log('yeaa touch');
+//        //need deselection
+//        console.log('yas');
+//        $('.cardholder').addClass('touchscreen');
 //        $('.card.card3').css('display', 'none');        
 //        $('.card.card2').css('display', 'none');
 //    };
-//    
-    document.addEventListener('touchstart', handleTouchStart, false);        
-    document.addEventListener('touchmove', handleTouchMove, false);
+    
+//    document.addEventListener('touchstart', handleTouchStart, false);        
+//    document.addEventListener('touchmove', handleTouchMove, false);
 
 });
 

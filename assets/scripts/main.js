94422665b1d688f757a9da69adaf0e8461898cd0
Omar Nema <omarwnema@gmail.com>
2016-07-26 09:01:04 -0400

    mobile restructure

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -188,6 +188,48 @@ function cardExit(){
             $('.card').unbind('mouseenter');
 };
 
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {     
+//    console.log('ontouchstart' in document.documentElement);
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+    
+    console.log( evt.touches[0].clientX);
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 function aboutTimeLineMouseover() {
     var phaseOrder = $(this).data('phaseorder');
     var currHover = $('.phaseHover.' + phaseOrder);
@@ -228,14 +270,6 @@ function aboutSecondClick(){
 
 };
 
-// function cardClick(){
-//     var title = $(this).attr('title');
-//     $('.content').fadeOut(200);
-//     $('.content').fadeOut(200, function(){
-//            window.location.href = 'projects/' + title ;
-//     });
-// };
-
 
 
 $(document).ready(function(event){
@@ -259,8 +293,20 @@ $(document).ready(function(event){
     //CARD FLIP
     $('.cardholder').mouseenter(cardEnter).mouseleave(cardExit);
     //changed to move accidentally?
+    
+    
+//    if (isTouchDevice){
+//        console.log('yeaa touch');
+//        $('.card.card3').css('display', 'none');        
+//        $('.card.card2').css('display', 'none');
+//    };
+//    
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
 
 });
+
+
 
 
 

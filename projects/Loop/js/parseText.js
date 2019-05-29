afcac275152c64b2cbbd18d6d800b5af71ca5a71
Omar <omar.nema@arcadiasolutions.com>
2019-05-28 21:19:05 -0400

    pseudo server side now

--- projects/Loop/js/parseText.js
+++ projects/Loop/js/parseText.js
@@ -1,7 +1,3 @@
-sentiment = new Sentimood();
-const colorPhrase = '#009688';
-const colorPerson = '#3498db';
-const colorEmotion = '#ef2971';
 
 function getIndicesOf(searchStr, str, caseSensitive) {
     var searchStrLen = searchStr.length;
@@ -21,7 +17,6 @@ function getIndicesOf(searchStr, str, caseSensitive) {
     }
     return indices;
 }
-
 
 function getTextIndices(inputRaw, phrases, numSections){
   //GET INDICES OF ALL PHRASES
@@ -122,104 +117,4 @@ function getTextIndices(inputRaw, phrases, numSections){
     }
   })
   return allPhrases;
-}
-
-function getHighlightFactors(input){
-  var me = false; var person = false; var place = false; var emotional = false; var color = 'black';
-  input['array'].forEach(function(d, i){
-    if (d['_text'] == 'i' || d['_text'] == 'me' || d['_text'] == 'my' || d['_text'] == 'mine'){
-      me = true;
-    }
-    if (d.tags['Person']){
-      person = true;
-      color = colorPerson;
-    }
-    if (d.tags['Place']){
-      place = true;
-    }
-    if (Math.abs(sentiment.analyze(d['_text'])['score']) > 2){
-      emotional = true;
-    }
-  })
-  if (emotional){
-    color = colorEmotion;
-  }
-  else if (person){
-    color = colorPerson;
-  }
-  else {
-    color = colorPhrase;
-  }
-
-  return {'text': input.string, 'me': me, 'person': person, 'place': place, 'emotional': emotional, 'color': color};
-}
-
-function getPhrases(doc){
-  phraseOutput = [];
-  doc.ngrams().list.forEach(function(e){
-    if (e.size == 2 && e.count > 2){
-      if (
-          (e.terms[0].tags['Noun'] && e.terms[1].tags['Noun'] && !e.terms[0].tags['Pronoun'] && !e.terms[1].tags['Pronoun'])
-          ||
-          (e.terms[0].tags['Adjective'] && e.terms[1].tags['Noun'] && !e.terms[0].tags['Pronoun'] && !e.terms[1].tags['Pronoun'])
-        ) {
-           phraseOutput.push(getHighlightFactors({string: e.key, array: e.terms}));
-        }
-
-    }
-    else if (e.size == 3 && e.count > 1){
-      //may benefit from verbs. i..e 'work on myself'
-      if
-          ( (e.terms[0].tags['Noun'] || e.terms[0].tags['Adjective']) && (e.terms[2].tags['Noun'] || e.terms[2].tags['Adjective'])
-            &&
-            !(e.terms[0].tags['Pronoun'] && e.terms[1].tags['QuestionWord'] && e.terms[2].tags['Pronoun']) //maybe ake this stricter and not have pronoun sandwich
-          )
-         {
-          phraseOutput.push(getHighlightFactors({string: e.key, array: e.terms}));
-        }
-
-    }
-    else if (e.size > 3 && e.count > 3){
-      return e.key;
-    }
-    // for unigrams, only take if high frequency and charged sentiment. not super effective. need to adjust library.
-    else if (e.size == 1 && e.count > 2 ){
-      highlightOutput = getHighlightFactors({string: e.key, array: e.terms});
-      if (highlightOutput['emotional'] || highlightOutput['person']){
-        phraseOutput.push(highlightOutput);
-      }
-    }
-  })
-  return phraseOutput;
-};
-
-function supplementVocabulary(doc){
-  doc.match('alisse').tag('Person', 'FemaleName');
-  doc.match('w').tag('Preposition');
-}
-
-//used to generate grams file
-
-function startSketch(inputRaw, numSections){
-
-  //NLP
-  console.log('running nlp on text...'); console.time('nlp processing time');
-  doc= nlp(inputRaw);
-  supplementVocabulary(doc);
-  console.timeEnd('nlp processing time');
-  console.log(doc);
-
-  //PARSE PHRASES
-  console.log('parsing phrases from text...'); console.time('phrase processing time');
-  grams = getPhrases(doc);
-  console.timeEnd('phrase processing time')
-
-
-
-  //CREATE D3 FRIENDLY ARRAY
-  console.log('create viz friendly array...'); console.time('viz processing time');
-  indexedArr = getTextIndices(inputRaw, grams, numSections);
-  console.timeEnd('viz processing time')
-  console.log(indexedArr)
-  return [indexedArr, inputRaw.length]
 }
